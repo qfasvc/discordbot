15e63fc0fabb51f52993110feba31977592ad9cc
qfasvc <wlsdnr8541@naver.com>
2021-07-12 01:34:42 +0900

    tlqkf 안해

--- index.js
+++ index.js
@@ -140,10 +140,6 @@ client.on('message', message => {
   if (message.content === '무야호') {
     message.channel.send('무야호');
   }
-  
-  if(message.content.startsWith(config.prefix + 'ping')) {
-    message.channel.send(client.ping + ' ms')
-  }
 
   // 입력한 값이 가위 또는 바위 또는 보일때
   if (message.content === "가위" || message.content === "바위" || message.content === "보") {
