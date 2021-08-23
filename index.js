554b6006bb98834cda197dfe1bb3a478129f1bed
qfasvc <wlsdnr8541@naver.com>
2021-08-23 22:03:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`http://localhost:${port}`));
-
 require('dotenv').config();
 const Discord = require('discord.js');
 const fs = require('fs');
